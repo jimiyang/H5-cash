c6105796c96a59401038d0387685948efd77a8c2
jimiyang <601285950@qq.com>
2018-08-10 17:46:41 +0800

    config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -24,12 +24,17 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
-  output: {
+  /*output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
+  }*/
+  output:{
+     path:path.resolve(__dirname,'./dist'),
+     publicPath:'/Hakkacash/dist',
+     filename:'build.js'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
